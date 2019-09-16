dc46150535e8c4a108e0300e792b840ce861401c
Haythem <i-tem@live.fr>
2019-09-16 10:59:07 +0200

    Initial commit

---
+++ src/main/java/com/creche/entities/Parents.java
@@ -0,0 +1,17 @@
+package com.creche.entities;
+
+import java.io.Serializable;
+
+public class Parents implements Serializable {
+
+    private String nomDeFamille;
+    private String prenom;
+    private String adressePostale;
+    private String codePostale;
+    private String ville;
+    private String telephone;
+    private String email;
+    private String telephone2;
+
+
+}
---
+++ src/main/java/com/creche/gennevilliers/GennevilliersApplication.java
@@ -0,0 +1,13 @@
+package com.creche.gennevilliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GennevilliersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GennevilliersApplication.class, args);
+    }
+
+}
