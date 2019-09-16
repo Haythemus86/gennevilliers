6628f05eb49dfb44afcfa3dd64bf95126b43ea93
Haythem <i-tem@live.fr>
2019-09-16 11:27:32 +0200

    Création de la Class Parents avec ses attributs

--- src/main/java/com/creche/entities/Parents.java
+++ src/main/java/com/creche/entities/Parents.java
@@ -13,5 +13,100 @@ public class Parents implements Serializable {
     private String email;
     private String telephone2;
 
+    //Constructeur par défaut
+
+    public Parents() {
+        super();
+    }
+
+    //Constructeur avec parametre
+
+    public Parents(String nomDeFamille, String prenom, String adressePostale, String codePostale, String ville, String telephone, String email, String telephone2) {
+        this.nomDeFamille = nomDeFamille;
+        this.prenom = prenom;
+        this.adressePostale = adressePostale;
+        this.codePostale = codePostale;
+        this.ville = ville;
+        this.telephone = telephone;
+        this.email = email;
+        this.telephone2 = telephone2;
+    }
+
+
+    //GETTERS
+
+    public String getNomDeFamille() {
+        return nomDeFamille;
+    }
+
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+
+    public String getAdressePostale() {
+        return adressePostale;
+    }
+
+
+    public String getCodePostale() {
+        return codePostale;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+    //SETTERS
+
+    public void setNomDeFamille(String nomDeFamille) {
+        this.nomDeFamille = nomDeFamille;
+    }
+
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setAdressePostale(String adressePostale) {
+        this.adressePostale = adressePostale;
+    }
+
+    public void setCodePostale(String codePostale) {
+        this.codePostale = codePostale;
+    }
+
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone2() {
+        return telephone2;
+    }
+
+    public void setTelephone2(String telephone2) {
+        this.telephone2 = telephone2;
+    }
 
 }
